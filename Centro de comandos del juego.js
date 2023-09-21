b31616cc4e0146b8d6932e9322d9fff0ccfbfbd4
LuciaRomeoGago <136198276+LuciaRomeoGago@users.noreply.github.com>
2023-09-21 19:19:57 -0300

    Add files via upload

---
+++ Centro de comandos del juego.js
@@ -0,0 +1,9 @@
+let preguntaIniciadora = prompt("¿En qué juego está interesado en jugar? Al ahorcado(1), adivina un número(2) o al ¿Quien soy? versión modelos(3).")
+let opciones = ["Ahorcado", "Adivina el número", "Quien soy?"]
+if (preguntaIniciadora == "Ahorcado" || "1") {
+    // El codigo del Juego, no??
+} else if (preguntaIniciadora == "Quien soy?" ||"3" ) {
+    // codigo del juego??¿
+} else {
+    //maybe codigo del juego
+}
