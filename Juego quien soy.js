bb365988c8fe569ff84719b83f40a6173009d0c1
LuciaRomeoGago <136198276+LuciaRomeoGago@users.noreply.github.com>
2023-09-21 18:39:11 -0300

    Add files via upload

---
+++ Juego quien soy.js
@@ -0,0 +1,22 @@
+let preguntaInicial = prompt("¿Su modelo es mayor de 30 años?")
+if (preguntaInicial == "Si") {
+let preguntaOjos = prompt("¿El color de sus ojos son: negros o azules?")
+        if (preguntaOjos == "Negros") {
+            alert("Usted ha elegido a Sora Choi!")
+        } else {
+            alert("Usted ha elegido a Irina Shayk!")
+        }
+    } else {
+        let preguntaEdad = prompt("¿Su modelo es menor de 27 años?")
+        if (preguntaEdad == "Si" ) {
+            alert("Usted a elegido a Isabella Hadid!")
+        } else {
+            let preguntaPelo = prompt("¿Su color de cabello es rubio o castaño?")
+        if (preguntaPelo == "rubio") {
+            alert("Usted ha elegido a Jelena Hadid!")
+        } else { 
+            alert("Usted ha elegido a Kendal Jenner!")
+        } }
+    }  
+        
+// Hay que alargarlo ASAP
