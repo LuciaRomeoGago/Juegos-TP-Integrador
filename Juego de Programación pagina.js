4a6b0f653ec3e5a3d099959158caea8eda4e4c84
LuciaRomeoGago <136198276+LuciaRomeoGago@users.noreply.github.com>
2023-09-21 14:27:32 -0300

    Update Juego de Programación pagina.js

--- Juego de Programación pagina.js
+++ Juego de Programación pagina.js
@@ -1,27 +1,34 @@
-const palabra = "promocionar";
-let intentos = 3;
-let adivinar = []
+const words = ["promocionar", "cursar","aprobar","final","parcial"];
+const word = words[Math.floor(Math.random()*words.length)];
+let hiddenWord="-".repeat(words.lenght);
+let tries = 6;
 
-while (intentos > 0 && adivinar !== palabra) {
-pregunta = prompt ("Ingrese una letra para adivinar la palabra: ");
 
-if (palabra.indexOf(pregunta) != -1) {
-        for (let i=0;i<palabra.length; i++){
-            if(palabra[i]==pregunta) 
-            console.log ("Esta la letra: " + pregunta +  "en la posición: " + i) // Tambien avisa si hay mas de una de esas letras
-        adivinar.push(pregunta) 
+while (tries > 0 && hiddenWord.includes("-")) {
+let letter = prompt ("Ingrese una letra: ");
+
+        console.log("Palabra: "+hiddenWord+".");
+        console.log("Le quedan: "+tries+" intentos.");
+if (!/^[a-zA-Z]+$/.test(letter)) { // desde la a minuscula hasta la Z mayuscula, lo demas NOP
+console.log(letter+" no es una letra, por favor vuelta a intentar: ");
+}
+else if (word.includes(letter)) {
+        for (let i = 0;i < word.length; i++){
+            if(word[i]=== letter) 
+          hiddenWord = hiddenWord.substring(0, i) + letter + hiddenWord.substring(i + 1); 
+        }
+      }
         //Deberia avisar y sustituir, DEBERIA!!
         } 
     } else { 
-        intentos -= 1
-        pregunta += 1 
-        console.log("No se encuentra esa letra, se le resta un intento, le quedan: " + intentos)
+        intentos --1
+        console.log("No se encuentra esa letra: "+ letter + ", se le resta un intento, le quedan: " + intentos)
     }
 }
 if (adivinar === palabra) {
-    console.log("Felicitaciones, ha ganado, la palabra era: "+palabra);
+    console.log("Felicitaciones, ha ganado, la palabra era: "+palabra+ "!");
 } else {
-    console.log ("Lo siento, perdió. La palabra era: " + palabra)
+    console.log ("Lo siento, perdió. La palabra era: " + palabra + ".");
 }
 
 
