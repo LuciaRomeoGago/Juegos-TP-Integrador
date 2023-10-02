a41e7bd1565328ed527525c9cf24bce8d9e368b2
LuciaRomeoGago <136198276+LuciaRomeoGago@users.noreply.github.com>
2023-10-02 15:10:53 -0300

    Update Adivina el nro.js

--- Adivina el nro.js
+++ Adivina el nro.js
@@ -1,6 +1,6 @@
 const number = Math.floor(Math.random() * 10)+1;
 let attempts = 0;
-let guess;
+let guess = 0;
 while(number !== guess) {
  guess= parseInt(prompt("Adivine el número, del 1 al 100!"));
  attempts++
@@ -8,8 +8,9 @@ while(number !== guess) {
     console.log("Por favor ingresa un número válido.")
 }else if (guess < number) {
     console.log("El número por adivinar es mayor al introducido: " + guess+ "!")
-} else {
+} else if (guess > number) {
     console.log("El número por adivinar es menor al introducido: " + guess + "!")
 }}
     console.log("Ha adivinado el número "+number+" en "+attempts+" intentos, felicitaciones!")
 // me hace todo bien pero por alguna razon me imprime el nro dos veces dentro y fuera de la consigna
+// update = le faltaba especificar en el ultimo else con un if
